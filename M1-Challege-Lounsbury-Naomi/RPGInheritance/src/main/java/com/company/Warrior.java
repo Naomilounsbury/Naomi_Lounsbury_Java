e597b3886e7cc96539b4eccbdedd9daab91a558e
Naomi Lounsbury <naomilounsbury@hotmail.com>
2022-07-05 21:34:48 -0700

    finished the warrior test

--- M1-Challege-Lounsbury-Naomi/RPGInheritance/src/main/java/com/company/Warrior.java
+++ M1-Challege-Lounsbury-Naomi/RPGInheritance/src/main/java/com/company/Warrior.java
@@ -1,11 +1,19 @@
 package com.company;
 
 public class Warrior extends BaseCharacter{
-    public void introduce(){
-        System.out.println("Hi my name is "+getName()+" and I am a warrior");
+    public String introduce(){
+        return "Hi my name is "+getName()+" and I am a warrior";
     }
 
     private int ShieldStrength;
+
+    public int getShieldStrength() {
+        return ShieldStrength;
+    }
+
+    public void setShieldStrength(int shieldStrength) {
+        ShieldStrength = shieldStrength;
+    }
 
     public Warrior(String name, int strength, int health, int stamina, int speed, int attackPower, boolean running, boolean arrested, int shieldStrength) {
         super(name, strength, health, stamina, speed, attackPower, running, arrested);
--- M1-Challege-Lounsbury-Naomi/RPGInheritance/src/test/java/com/company/WarriorTest.java
+++ M1-Challege-Lounsbury-Naomi/RPGInheritance/src/test/java/com/company/WarriorTest.java
@@ -8,5 +8,36 @@ public class WarriorTest {
 
     @Test
     public void shouldIntroduceThemself() {
+        Warrior guy= new Warrior("lina", 75, 100,75, 10, 1,false, false,50);
+        String expectedValue ="Hi my name is "+guy.getName()+ " and I am a warrior";
+
+        //act
+        String actualValue = guy.introduce();
+
+        //Assert
+        assertEquals(expectedValue, actualValue);
     }
+    @Test
+    public void shouldGetShieldStrength() {
+        Warrior guy= new Warrior("lina", 75, 100,75, 10, 1,false, false,50);
+        int expectedValue =50;
+
+        //act
+        int actualValue = guy.getShieldStrength();
+
+        //Assert
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test()
+    public void shouldSetShieldStrength() {
+        Warrior guy= new Warrior("lina", 75, 100,75, 10, 1,false, false,50);
+        int expectedValue =75;
+        guy.setShieldStrength(75);
+        //act
+        int actualValue = guy.getShieldStrength();
+
+        //Assert
+        assertEquals(expectedValue, actualValue);
+    }
+
 }
