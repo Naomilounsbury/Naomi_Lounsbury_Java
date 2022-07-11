6db17b4d22ea72bbb85c9565320af85632cc3f7e
Naomi Lounsbury <naomilounsbury@hotmail.com>
2022-07-10 21:50:57 -0700

    more code

---
+++ M2-Challenge-Lounsbury-Naomi/src/main/java/com/company/M2ChallengeLounsburyNaomi/M2ChallengeLounsburyNaomiApplication.java
@@ -0,0 +1,13 @@
+package com.company.M2ChallengeLounsburyNaomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2ChallengeLounsburyNaomiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2ChallengeLounsburyNaomiApplication.class, args);
+	}
+
+}
---
+++ M2-Challenge-Lounsbury-Naomi/src/test/java/com/company/M2ChallengeLounsburyNaomi/M2ChallengeLounsburyNaomiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.M2ChallengeLounsburyNaomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2ChallengeLounsburyNaomiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
