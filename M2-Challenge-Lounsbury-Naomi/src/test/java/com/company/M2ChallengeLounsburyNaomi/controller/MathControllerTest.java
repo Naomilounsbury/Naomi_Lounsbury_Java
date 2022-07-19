d31d0b00ef5808cff71719b7c6699b3a70d977da
Naomi Lounsbury <naomilounsbury@hotmail.com>
2022-07-19 14:41:25 -0700

    more tests

--- M2-Challenge-Lounsbury-Naomi/src/test/java/com/company/M2ChallengeLounsburyNaomi/controller/MathControllerTest.java
+++ M2-Challenge-Lounsbury-Naomi/src/test/java/com/company/M2ChallengeLounsburyNaomi/controller/MathControllerTest.java
@@ -36,27 +36,7 @@ public class MathControllerTest {
         // objects. We don't have to do anything special for mockMvc since it's Autowired. We will however be using
         // setUp() in the future.
     }
-    @Test
-    public void shouldReturn422StatusCodeWithInvalidRequestBodyForAdd()throws Exception{
-            // ARRANGE
-            MathSolution math = new MathSolution();
-            math.setOperand1(5);
-            math.setOperand2(8);
 
-
-            // c
-            String inputJson = mapper.writeValueAsString(math);
-
-            // ACT
-            mockMvc.perform(
-                            post("/add")                                // Perform the POST request.
-                                    .content(inputJson)                               // Set the request body.
-                                    .contentType(MediaType.APPLICATION_JSON)          // Tell the server it's in JSON format.
-                    )
-                    .andDo(print())                                           // Print results to console.
-                    .andExpect(status().isUnprocessableEntity());             // ASSERT (status code is 422)
-
-    }
     @Test
     public void shouldReturn422StatusCodeForInputTypeOfZeroAdd()throws Exception{
         // ARRANGE
@@ -269,6 +249,72 @@ public class MathControllerTest {
         // ACT
         this.mockMvc.perform(
                         post("/add")                                // Perform the POST request.
+                                .content(inputJson)                               // Set the request body.
+                                .contentType(MediaType.APPLICATION_JSON)          // Tell the server it's in JSON format.
+                )
+                .andDo(print())                                           // Print results to console.
+                .andExpect(status().isUnprocessableEntity());             // ASSERT (status code is 422)
+
+    }
+    @Test
+    public void shouldReturn422StatusCodeForStringInputsSubtract()throws Exception{
+        // ARRANGE
+        Map<String, String> input=new HashMap<>();
+        input.put("operand1", "5");
+        input.put("operand2", "this is a string");
+
+
+
+
+        String inputJson = mapper.writeValueAsString(input);
+
+        // ACT
+        this.mockMvc.perform(
+                        post("/subtract")                                // Perform the POST request.
+                                .content(inputJson)                               // Set the request body.
+                                .contentType(MediaType.APPLICATION_JSON)          // Tell the server it's in JSON format.
+                )
+                .andDo(print())                                           // Print results to console.
+                .andExpect(status().isUnprocessableEntity());             // ASSERT (status code is 422)
+
+    }
+    @Test
+    public void shouldReturn422StatusCodeForStringInputsMultiply()throws Exception{
+        // ARRANGE
+        Map<String, String> input=new HashMap<>();
+        input.put("operand1", "5");
+        input.put("operand2", "this is a string");
+
+
+
+
+        String inputJson = mapper.writeValueAsString(input);
+
+        // ACT
+        this.mockMvc.perform(
+                        post("/multiply")                                // Perform the POST request.
+                                .content(inputJson)                               // Set the request body.
+                                .contentType(MediaType.APPLICATION_JSON)          // Tell the server it's in JSON format.
+                )
+                .andDo(print())                                           // Print results to console.
+                .andExpect(status().isUnprocessableEntity());             // ASSERT (status code is 422)
+
+    }
+    @Test
+    public void shouldReturn422StatusCodeForStringInputsDivide()throws Exception{
+        // ARRANGE
+        Map<String, String> input=new HashMap<>();
+        input.put("operand1", "5");
+        input.put("operand2", "this is a string");
+
+
+
+
+        String inputJson = mapper.writeValueAsString(input);
+
+        // ACT
+        this.mockMvc.perform(
+                        post("/divide")                                // Perform the POST request.
                                 .content(inputJson)                               // Set the request body.
                                 .contentType(MediaType.APPLICATION_JSON)          // Tell the server it's in JSON format.
                 )
